b021d87a5ded02bc251385fe7bd7f9e10f42f504
OGMaksiu <mateckimaksymilian@gmail.com>
2022-12-07 14:58:54 +0100

    dodanie menu w fragmencie 1 zaczecie pracy nad ui fragmentu 2

--- app/src/main/java/com/barstool/cockwojaer/MainActivity.kt
+++ app/src/main/java/com/barstool/cockwojaer/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.barstool.cockwojaer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var activityUser = findViewById<Button>(R.id.btnUser)
+        var activityRandom = findViewById<Button>(R.id.btnRand)
+        var closeBtn = findViewById<Button>(R.id.btnCls)
+
+
+        activityUser.setOnClickListener(){
+             val intent =  Intent(this, UserActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/barstool/cockwojaer/UserActivity.kt
@@ -0,0 +1,21 @@
+package com.barstool.cockwojaer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+        val map = arrayOf(
+            intArrayOf(),
+            intArrayOf(),
+            intArrayOf(),
+            intArrayOf(),
+            intArrayOf(),
+            intArrayOf(),
+            intArrayOf(),
+            intArrayOf(),
+        )
+    }
+}
