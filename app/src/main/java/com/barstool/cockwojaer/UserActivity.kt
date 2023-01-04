d7a779e1e204e7f916305150bde3f7981b92554c
Michał Piaskowski <M.Piaskowski@student.zschie.edu.pl>
2023-01-04 20:48:33 +0100

    przebudowanie user activity piersze testy

--- app/src/main/java/com/barstool/cockwojaer/UserActivity.kt
+++ app/src/main/java/com/barstool/cockwojaer/UserActivity.kt
@@ -12,6 +12,67 @@ import java.util.*
 import kotlin.math.min
 import com.barstool.cockwojaer.algorithm as algorithm
 
+fun <T: Comparable<T>> nextPermutation(vector: MutableList<T>, first: Int, last: Int): Boolean {
+    if (first == last) {
+        return false
+    }
+
+    var i = last
+    while (i > first && vector[i - 1] >= vector[i]) {
+        i--
+    }
+
+    if (i == first) {
+        return false
+    }
+
+    var j = last
+    while (vector[j] <= vector[i - 1]) {
+        j--
+    }
+
+    val temp = vector[i - 1]
+    vector[i - 1] = vector[j]
+    vector[j] = temp
+
+    j = last
+    while (i < j) {
+        val temp2 = vector[i]
+        vector[i] = vector[j]
+        vector[j] = temp2
+        i++
+        j--
+
+    }
+
+    return true
+}
+fun shortest_path_sum(edges_list:Array<IntArray>) :Int{
+    var source = 0
+
+    val nodes = Vector<Int>()
+
+    for (i in 0..7) {
+        if (i != source) {
+            nodes.add(i)
+        }
+
+    }
+
+    var shortest_path = Int.MAX_VALUE
+
+    while (nextPermutation(nodes, nodes.firstElement(), nodes.lastElement())){
+        var path_weight = 0
+        var j = source
+        for(n in nodes){
+            path_weight += edges_list[j][nodes[n]]
+            j = nodes[n]
+        }
+        path_weight += edges_list[j][source]
+        shortest_path = min(shortest_path, path_weight).toInt()
+    }
+    return shortest_path
+}
 
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +91,7 @@ class UserActivity : AppCompatActivity() {
 
 
 
+
         dodajBtn.setOnClickListener {
           edges_list[originSpinner.selectedItemPosition][destinationSpinner.selectedItemPosition] = odleglosc.text.toString().toInt()
             edges_list[destinationSpinner.selectedItemPosition][originSpinner.selectedItemPosition] = odleglosc.text.toString().toInt()
@@ -43,7 +105,8 @@ class UserActivity : AppCompatActivity() {
 
                 test.append(row.contentToString() + '\n')*/
           //test.setText()
-
+            var it = shortest_path_sum(edges_list)
+            test.text = it.toString()
 
         }
 
