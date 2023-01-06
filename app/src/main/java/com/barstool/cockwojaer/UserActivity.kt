7e5c4af917048714edf304a3452f1f22d396360e
Michał Piaskowski <M.Piaskowski@student.zschie.edu.pl>
2023-01-06 22:15:29 +0100

    wysczyszczenie ui oraz dodanie działającego algorytmu

--- app/src/main/java/com/barstool/cockwojaer/UserActivity.kt
+++ app/src/main/java/com/barstool/cockwojaer/UserActivity.kt
@@ -1,32 +1,31 @@
 package com.barstool.cockwojaer
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.content.Intent
-import android.icu.lang.UCharacter
 import android.widget.*
 import kotlinx.android.synthetic.main.activity_user.*
-import kotlinx.coroutines.withTimeout
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.math.min
-import com.barstool.cockwojaer.algorithm as algorithm
 
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
         var dodajBtn = findViewById<Button>(R.id.dodajBtn)
-        var arrayBtn = findViewById<Button>(R.id.arrayButton)
-        var testBtn = findViewById<Button>(R.id.button2)
+
+
         var originSpinner = findViewById<Spinner>(R.id.originS)
         var odleglosc = findViewById<EditText>(R.id.odległośćET)
         var destinationSpinner = findViewById<Spinner>(R.id.destinationS)
-        var test = findViewById<TextView>(R.id.textView3)
+
         var result = findViewById<TextView>(R.id.result)
         var num_nodes = 8
         var num_edges = 64
         var edges_list = Array(num_nodes){IntArray(num_nodes)}
+
+        fun isNumeric(str: String): Boolean = str
+            .removePrefix("-")
+            .removePrefix("+")
+            .all { it in '0'..'9' }
 
         fun liczenieTrasy(){
 
@@ -63,7 +62,11 @@ class UserActivity : AppCompatActivity() {
 
 
             for (i in 0 until cities) {
-                result.append("${shortestPath[i]} >> ")
+                if(i == cities-1){
+                    result.append("${shortestPath[i]}")
+                }else {
+                    result.append("${shortestPath[i]} >> ")
+                }
             }
 
 
@@ -78,22 +81,29 @@ class UserActivity : AppCompatActivity() {
 
 
         dodajBtn.setOnClickListener {
-          edges_list[originSpinner.selectedItemPosition][destinationSpinner.selectedItemPosition] = odleglosc.text.toString().toInt()
-            edges_list[destinationSpinner.selectedItemPosition][originSpinner.selectedItemPosition] = odleglosc.text.toString().toInt()
-          var toast =  Toast.makeText(baseContext,"dodano",Toast.LENGTH_SHORT)
-            toast.show()
+            if(originSpinner.selectedItemPosition == destinationSpinner.selectedItemPosition){
+                var toast = Toast.makeText(baseContext, "Nie możesz ustawić odległości do tego samego miasta", Toast.LENGTH_SHORT)
+                toast.show()
+
+            }else if (isNumeric(odleglosc.text.toString())){
+
+                edges_list[originSpinner.selectedItemPosition][destinationSpinner.selectedItemPosition] =
+                    odleglosc.text.toString().toInt()
+                edges_list[destinationSpinner.selectedItemPosition][originSpinner.selectedItemPosition] =
+                    odleglosc.text.toString().toInt()
+                var toast = Toast.makeText(baseContext, "Dodano odległość", Toast.LENGTH_SHORT)
+                toast.show()
+            }else{
+
+                var toast = Toast.makeText(baseContext, "Można wprowadzić tylko liczby", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
+        algoBtn.setOnClickListener {
+            result.text =""
+            liczenieTrasy()
 
         }
-        button2.setOnClickListener {
-            //liczenieTrasy()
-            Toast.makeText(getBaseContext(), "Reason can not be blank", Toast.LENGTH_SHORT).show()
-        }
-        arrayBtn.setOnClickListener {
-            test.setText("")
-            for (row in edges_list) {
 
-            test.append(row.contentToString() + '\n')
-        }
-        }
 
 }}
