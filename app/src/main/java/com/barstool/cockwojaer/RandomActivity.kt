7c864de4c6f80b5e501fc42a79be96aa43cfc5a5
Michał Piaskowski <M.Piaskowski@student.zschie.edu.pl>
2023-01-06 22:24:25 +0100

    doadnie random activity

---
+++ app/src/main/java/com/barstool/cockwojaer/RandomActivity.kt
@@ -0,0 +1,11 @@
+package com.barstool.cockwojaer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RandomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random)
+    }
+}
