9ee6d7150b1c0983359dc27601528aab2c9262bd
Michał Piaskowski <M.Piaskowski@student.zschie.edu.pl>
2023-01-06 22:26:50 +0100

    dodanie layout i skryptu do wprowadzania losowych odległości

--- app/src/main/java/com/barstool/cockwojaer/RandomActivity.kt
+++ app/src/main/java/com/barstool/cockwojaer/RandomActivity.kt
@@ -1,11 +1,106 @@
 package com.barstool.cockwojaer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_user.*
+import kotlin.random.Random
 
 class RandomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random)
-    }
-}
+        var dodajBtn = findViewById<Button>(R.id.dodajBtn)
+
+
+        var originSpinner = findViewById<Spinner>(R.id.originS)
+
+        var destinationSpinner = findViewById<Spinner>(R.id.destinationS)
+
+        var result = findViewById<TextView>(R.id.result)
+        var num_nodes = 8
+        var num_edges = 64
+        var edges_list = Array(num_nodes){IntArray(num_nodes)}
+
+        fun isNumeric(str: String): Boolean = str
+            .removePrefix("-")
+            .removePrefix("+")
+            .all { it in '0'..'9' }
+
+        fun liczenieTrasy(){
+
+
+            val cities = 8
+            val visitedCities = BooleanArray(cities)
+            val shortestPath = IntArray(cities)
+            var currentCity = 0
+            visitedCities[currentCity] = true
+            for (i in 0 until cities - 1) {
+                var minDistance = Int.MAX_VALUE
+
+
+                var leastDistance = 0
+
+
+                for (j in 0 until cities) {
+                    if (!visitedCities[j] && edges_list[currentCity][j] < minDistance) {
+                        minDistance = edges_list[currentCity][j]
+                        leastDistance = j
+                    }
+                }
+
+
+                shortestPath[i] = leastDistance
+                visitedCities[leastDistance] = true
+
+
+                currentCity = leastDistance
+            }
+
+
+            shortestPath[cities - 1] = 0
+
+
+            for (i in 0 until cities) {
+                if(i == cities-1){
+                    result.append("${shortestPath[i]}")
+                }else {
+                    result.append("${shortestPath[i]} >> ")
+                }
+            }
+
+
+            var cost = 0
+            for (i in 0 until cities - 1) {
+                cost += edges_list[shortestPath[i]][shortestPath[i + 1]]
+            }
+            result.append("\nŁączny pokonany dystans: $cost km")
+        }
+
+
+
+
+        dodajBtn.setOnClickListener {
+            if(originSpinner.selectedItemPosition == destinationSpinner.selectedItemPosition){
+                var toast = Toast.makeText(baseContext, "Nie możesz ustawić odległości do tego samego miasta", Toast.LENGTH_SHORT)
+                toast.show()
+
+            }else{
+
+                edges_list[originSpinner.selectedItemPosition][destinationSpinner.selectedItemPosition] = Random.nextInt(0, 100)
+
+                edges_list[destinationSpinner.selectedItemPosition][originSpinner.selectedItemPosition] = Random.nextInt(0, 100)
+
+                var toast = Toast.makeText(baseContext, "Dodano odległość", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
+        algoBtn.setOnClickListener {
+            result.text =""
+            liczenieTrasy()
+
+        }
+
+
+    }}
